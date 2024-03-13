a5db5a98c8dd8e9b5b57ca0181faad3b280d9609
tovo <tommy.vo@axaz.com>
2024-03-13 11:12:14 +0100

    Added dotenc package, added homepage.pug, modified homepage endpoint

--- index.js
+++ index.js
@@ -2,26 +2,31 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+require('dotenv').config()
+
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 app.get('/', async (req, res) => {
-    const pals = 'https://api.hubspot.com/crm/v3/objects/contacts';
+    const pals = 'https://api.hubspot.com/crm/v3/objects/pals?properties=name,no,element';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     }
     try {
-        const resp = await axios.get(contacts, { headers });
+        const resp = await axios.get(pals, { headers });
         const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        console.log(data);
+        res.render('homepage', { title: 'Homepage', data });      
     } catch (error) {
         console.error(error);
     }
@@ -30,15 +35,15 @@ app.get('/', async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 app.get('/update-cobj', async (req, res) => {
-    const pals = 'https://api.hubspot.com/crm/v3/objects/contacts';
+    const pals = 'https://api.hubspot.com/crm/v3/objects/pal?properties=name,no,element';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     }
     try {
-        const resp = await axios.get(contacts, { headers });
+        const resp = await axios.get(pals, { headers });
         const data = resp.data.results;
-        res.render('contacts', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });      
+        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });      
     } catch (error) {
         console.error(error);
     }
@@ -51,7 +56,7 @@ app.post('/update-cobj"', async (req, res) => {
         properties: {
             "name": req.body.newVal,
             "no": req.body.newVal,
-            "type": req.body.newVal
+            "element": req.body.newVal
         }
     }
 
